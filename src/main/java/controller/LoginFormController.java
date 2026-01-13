2e1cbe7137c998e849b502984b5c3d58406163b8
nisalmissaka <wishwakanisal@gmail.com>
2026-01-13 11:54:46 +0530

    create to signIn Button

---
+++ src/main/java/controller/LoginFormController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginFormController {
+
+    @FXML
+    private PasswordField PassWord;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    void btnSignInOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignUpOnAction(ActionEvent event) {
+
+    }
+
+}
